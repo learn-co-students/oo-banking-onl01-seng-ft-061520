242f1e3e069c30b9f36c656ccf97ec88bdcb0239
devWRM <mcwen@comcast.net>
2020-06-28 20:17:54 -0400

    Done.

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -18,12 +18,13 @@ class Transfer
 
   def execute_transaction 
 
-    if sender.balance < amount
-      return "Transaction rejected. Please check your account balance."    
-    elsif self.valid? && status == "pending"
+    if sender.balance > amount self.valid? && status == "pending"
       sender.balance = sender.balance - amount
       receiver.balance = receiver.balance + amount
       self.status = "complete"
+       
+    else
+      return "Transaction rejected. Please check your account balance."   
     end
 
   end
