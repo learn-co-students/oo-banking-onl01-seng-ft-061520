35a32927ccb0bbb78ba0c0841932b2add59e65d6
Shirlen Detablan <slen.detablan@gmail.com>
2020-07-01 05:02:28 +0000

    Done.

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,26 @@
 class Transfer
-  # your code here
+  
+  attr_accessor :status 
+  attr_reader :sender, :receiver, :amount
+  
+  
+  def initialize(sender, receiver, amount)
+    @sender = sender 
+    @receiver = receiver 
+    @amount = amount
+    @status = "pending"
+  end 
+  
+  def valid?
+    sender.valid? && receiver.valid?
+  end 
+  
+  def execute_transaction
+  end 
+  
+  def reverse_transfer
+  end 
+  
+  
+
 end
