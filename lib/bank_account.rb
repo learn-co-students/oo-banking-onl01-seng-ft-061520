2915aadcc7100f39a4fa07bf8cc8373c494c8de8
kak79 <kohelka@gmail.com>
2020-07-04 06:40:14 -0500

    Done.

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,31 @@
 class BankAccount
+    attr_accessor  :status, :balance
+    attr_writer
+    attr_reader :name
+
+    @@all = []
+
+    def initialize(name)
+        @name = name
+        @status = "open"
+        @balance = 1000
+    end
+
+    def self.all
+        @@all
+    end
+
+    def deposit(deposit_amt)
+        self.balance += deposit_amt
+    end
+
+    def display_balance
+        "Your balance is $#{self.balance}."
+    end
+
+    def valid?
+        self.one?[{}]
+    end
+
 
 end
--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,21 @@
 class Transfer
-  # your code here
+  attr_accessor  
+  attr_writer
+  attr_reader 
+
+  @@all = []
+
+  def initialize
+  
+  end
+
+  def self.all
+      @@all
+  end
+
+  
+
+
+
+
 end
