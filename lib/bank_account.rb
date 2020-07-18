7d9e270587a4bc9db179e2c80fcabb38c50b3ae2
Brodie Curtis <curtis.brodie1011@yahoo.com>
2020-07-18 17:09:27 +0000

    Done.

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,48 @@
+require 'pry'
 class BankAccount
 
+
+  attr_accessor :balance, :status
+  attr_reader :name
+  
+  def initialize(name)
+    @name = name
+    @balance = 1000
+    @status = "open"
+  end
+  
+  def deposit(deposit)
+    # add money to current balance
+  
+    self.balance += deposit
+  end
+  
+  def display_balance
+    "Your balance is $#{self.balance}."
+  end
+  
+  def valid?
+    if self.balance == 0
+      false
+    elsif self.status == "closed"
+      false
+    else
+      true
+    end
+  end
+  
+  def close_account
+    @status = "closed"
+  end
 end
+
+
+
+
+
+
+
+
+
+
+
